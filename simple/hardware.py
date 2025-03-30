8d6119f0f8e252b4e343d2796aa19b38f5185cbf
tx42 <Skulimovskyy.bogdan@gmail.com>
2025-03-30 08:04:19 +0100

    uncomment hardware.py

--- simple/hardware.py
+++ simple/hardware.py
@@ -1,99 +1,99 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import cv2 as cv
 
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-# class Motors():
-#     def __init__(self, enA, in1, in2, enB, in3, in4):
-#         self.enable_a = enA
-#         self.enable_b = enB
+class Motors():
+    def __init__(self, enA, in1, in2, enB, in3, in4):
+        self.enable_a = enA
+        self.enable_b = enB
 
-#         self.in_1 = in1
-#         self.in_2 = in2
-#         self.in_3 = in3
-#         self.in_4 = in4
+        self.in_1 = in1
+        self.in_2 = in2
+        self.in_3 = in3
+        self.in_4 = in4
 
-#         GPIO.setup(self.enable_a,GPIO.OUT)
-#         GPIO.setup(self.enable_b,GPIO.OUT)
+        GPIO.setup(self.enable_a,GPIO.OUT)
+        GPIO.setup(self.enable_b,GPIO.OUT)
 
-#         GPIO.setup(self.in_1,GPIO.OUT) # set gpio pins as outputs
-#         GPIO.setup(self.in_2,GPIO.OUT)
-#         GPIO.setup(self.in_3,GPIO.OUT)
-#         GPIO.setup(self.in_4,GPIO.OUT)
+        GPIO.setup(self.in_1,GPIO.OUT) # set gpio pins as outputs
+        GPIO.setup(self.in_2,GPIO.OUT)
+        GPIO.setup(self.in_3,GPIO.OUT)
+        GPIO.setup(self.in_4,GPIO.OUT)
 
 
-#         self.stop()
-#         self.left=GPIO.PWM(self.enable_a,1000)
-#         self.right=GPIO.PWM(self.enable_b,1000) # set pwm freq
+        self.stop()
+        self.left=GPIO.PWM(self.enable_a,1000)
+        self.right=GPIO.PWM(self.enable_b,1000) # set pwm freq
 
-#         self.left.start(25)
-#         self.right.start(25)
+        self.left.start(25)
+        self.right.start(25)
 
-#     def forward(self):
-#         GPIO.output(self.in_1,GPIO.HIGH)
-#         GPIO.output(self.in_2,GPIO.LOW)
+    def forward(self):
+        GPIO.output(self.in_1,GPIO.HIGH)
+        GPIO.output(self.in_2,GPIO.LOW)
 
-#         GPIO.output(self.in_3,GPIO.LOW)
-#         GPIO.output(self.in_4,GPIO.HIGH)
+        GPIO.output(self.in_3,GPIO.LOW)
+        GPIO.output(self.in_4,GPIO.HIGH)
     
-#     def back(self):
-#         GPIO.output(self.in_1,GPIO.LOW)
-#         GPIO.output(self.in_2,GPIO.HIGH)
+    def back(self):
+        GPIO.output(self.in_1,GPIO.LOW)
+        GPIO.output(self.in_2,GPIO.HIGH)
 
-#         GPIO.output(self.in_3,GPIO.HIGH)
-#         GPIO.output(self.in_4,GPIO.LOW)
+        GPIO.output(self.in_3,GPIO.HIGH)
+        GPIO.output(self.in_4,GPIO.LOW)
 
-#     def right(self): 
-#         GPIO.output(self.in_1,GPIO.HIGH)
-#         GPIO.output(self.in_2,GPIO.LOW)
+    def right(self): 
+        GPIO.output(self.in_1,GPIO.HIGH)
+        GPIO.output(self.in_2,GPIO.LOW)
 
-#         GPIO.output(self.in_3,GPIO.HIGH)
-#         GPIO.output(self.in_4,GPIO.LOW)
+        GPIO.output(self.in_3,GPIO.HIGH)
+        GPIO.output(self.in_4,GPIO.LOW)
     
-#     def left(self):
-#         GPIO.output(self.in_1,GPIO.LOW)
-#         GPIO.output(self.in_2,GPIO.HIGH)
+    def left(self):
+        GPIO.output(self.in_1,GPIO.LOW)
+        GPIO.output(self.in_2,GPIO.HIGH)
 
-#         GPIO.output(self.in_3,GPIO.LOW)
-#         GPIO.output(self.in_4,GPIO.HIGH)
+        GPIO.output(self.in_3,GPIO.LOW)
+        GPIO.output(self.in_4,GPIO.HIGH)
 
-#     def stop(self):
-#         GPIO.output(self.in_1, GPIO.LOW)
-#         GPIO.output(self.in_2, GPIO.LOW)
-#         GPIO.output(self.in_3, GPIO.LOW)
-#         GPIO.output(self.in_4, GPIO.LOW)
+    def stop(self):
+        GPIO.output(self.in_1, GPIO.LOW)
+        GPIO.output(self.in_2, GPIO.LOW)
+        GPIO.output(self.in_3, GPIO.LOW)
+        GPIO.output(self.in_4, GPIO.LOW)
 
-#     def speed(self, speed):
-#         self.left.ChangeDutyCycle(speed)
-#         self.right.ChangeDutyCycle(speed)
+    def speed(self, speed):
+        self.left.ChangeDutyCycle(speed)
+        self.right.ChangeDutyCycle(speed)
 
-#     def coast(self):
-#         self.left.ChangeDutyCycle(0)
-#         self.right.ChangeDutyCycle(0)
+    def coast(self):
+        self.left.ChangeDutyCycle(0)
+        self.right.ChangeDutyCycle(0)
 
-# class Servo:
-#     def __init__(self, pin):
-#         self.pin = pin
+class Servo:
+    def __init__(self, pin):
+        self.pin = pin
 
-#         GPIO.setup(pin, GPIO.OUT)
-#         # 50 Hz (20 ms PWM period)
-#         self.pwm = GPIO.PWM(pin, 50)
-#         self.pwm.start(7)
+        GPIO.setup(pin, GPIO.OUT)
+        # 50 Hz (20 ms PWM period)
+        self.pwm = GPIO.PWM(pin, 50)
+        self.pwm.start(7)
     
-#     def max(self):
-#         self.pwm.ChangeDutyCycle(12.0) # rotate to 180 degrees
+    def max(self):
+        self.pwm.ChangeDutyCycle(12.0) # rotate to 180 degrees
 
-#     """
-#     rotate to 90 degrees
-#     """
-#     def up(self):
-#         self.pwm.ChangeDutyCycle(7.0) 
+    """
+    rotate to 90 degrees
+    """
+    def up(self):
+        self.pwm.ChangeDutyCycle(7.0) 
 
 
-#     # Zero the servo
-#     def down(self):
-#         self.pwm.ChangeDutyCycle(2.0) 
+    # Zero the servo
+    def down(self):
+        self.pwm.ChangeDutyCycle(2.0) 
 
-# motors = Motors(25, 24, 23, 11, 10, 9)
-# servo = Servo(17)
+motors = Motors(25, 24, 23, 11, 10, 9)
+servo = Servo(17)
 camera = cv.VideoCapture(0)
